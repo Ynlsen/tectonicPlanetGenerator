a8536745f679faaa48436b28700e062f281ec8bd
Ynlsen <yannick.niederer@shinternet.ch>
2025-06-08 16:39:20 +0200

    changed seed

--- scripts/ShapeSettings.cs
+++ scripts/ShapeSettings.cs
@@ -4,8 +4,8 @@ using System;
 [Tool]
 public partial class ShapeSettings : Resource
 {
-	[Export] public int Seed = 42;
-	[Export] public float NoiseScale = 1f;      // how “zoomed in” the noise is
-	[Export] public float MinHeight = 0f;       // displacement at noise = 0
-	[Export] public float MaxHeight = 0.2f;     // displacement at noise = 1
+	[Export] public int Seed = 69;
+	[Export] public float NoiseScale = 1f;
+	[Export] public float MinHeight = 0f;
+	[Export] public float MaxHeight = 0.2f;
 }
