16c22f80e9830d63d7ff8c48cefd7c0e415c18be
Ynlsen <yannick.niederer@shinternet.ch>
2025-06-11 01:21:34 +0200

    rewording

--- scripts/TectonicSettings.cs
+++ scripts/TectonicSettings.cs
@@ -7,8 +7,8 @@ public partial class TectonicSettings : Resource
   [Export] public int SmallPlateCount = 4;
 
   [Export] public int LargeSubPoints = 20;
-  [Export] public float LargeJitterAngle = 20f;
+  [Export] public float LargeDeviationAngle = 20f;
 
   [Export] public int SmallSubPoints = 2;
-  [Export] public float SmallJitterAngle = 5f;
+  [Export] public float SmallDeviationAngle = 5f;
 }
